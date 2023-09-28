4befca8bcccec593642c1e9ca0893241e089669c
Nacho2070 <ignaciomrtn90@gmail.com>
2023-09-28 19:34:41 -0300

    actualizado

--- Ejercicios_condicionales2.py
+++
@@ -1,9 +0,0 @@
-entrada_fecha = input("Ingrese la fecha en formato 'día, DD/MM': ").lower()  # Convertir a minúsculas
-datos_fecha = entrada_fecha.split(', ')
-if len(datos_fecha) != 2:
-    print("Error: Formato de fecha incorrecto.")
-else:
-    dia_semana, fecha_num = datos_fecha[0], datos_fecha[1]
-    dia, mes = fecha_num.split('/')
-    dia = int(dia)
-    mes = int(mes)
